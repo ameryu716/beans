5500cc83d92f9b0d0c6037adf2dcb82f2819666e
ameryu716 <72952772+ameryu716@users.noreply.github.com>
2021-02-09 11:24:36 +0900

    add

--- ShogiPieces/piece.js
+++ ShogiPieces/piece.js
@@ -13,6 +13,8 @@ window.addEventListener("load",()=>{
     const leftmargerspan = document.getElementById("margerleftspan");
     const sizer = document.getElementById("sizer");
 
+    const screenshot = document.getElementById("screenshot");
+
 
     let degr =20;
     let piecedirecNum = 1;
@@ -73,5 +75,20 @@ window.addEventListener("load",()=>{
     leftmarger.onchange = ()=>{
         document.getElementsByClassName("surface1")[0].getElementsByTagName("span")[0].style.paddingLeft = leftmarger.value + "%";
         leftmargerspan.innerText = leftmarger.value + "%";
+    }
+
+    screenshot.onclick = ()=>{
+        const main = document.getElementsByTagName("main");
+        const randomStr5 = String(Math.floor(Math.random()*99999)+1).substr(0,5);//ランダム5
+        
+        html2canvas(main[0])
+        .then((canvas)=>{
+            canvas.crossOrigin = "Anonymous";
+            //保存
+            const dc = document.createElement("a");
+            dc.download = "shogikoma"+randomStr5+".png";
+            dc.href = canvas.toDataURL("shogikoma"+randomStr5+"/png");
+            dc.click();
+        })
     }
 })
